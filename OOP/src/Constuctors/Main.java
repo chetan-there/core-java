d7afd73062a6fc9f73077be3fe4a858dd33dd554
chetan-there <therechetan24@gmail.com>
2025-03-31 12:25:23 +0530

    constructor

---
+++ OOP/src/Constuctors/Main.java
@@ -0,0 +1,37 @@
+package OOP.src.Constuctors;
+
+//Constructor = special method that is called when an object is instantiated (Created)
+
+        //A constructor in Java is a special method used to initialize objects. It has the same name as the class and is automatically called when an object is created.
+        //Constructors are allowed to assign different attributes to each object that we instantiated objects .
+
+        //         Rules for Constructors in Java
+        
+        // Constructor name must be the same as the class name.
+        // No return type (not even void).
+        // Automatically called when an object is created.
+        // Can be overloaded (multiple constructors in the same class).
+        // If no constructor is defined, Java provides a default constructor.
+
+        // Types of Constructors in Java
+        
+        // Default Constructor (No parameters)==Automatically called when an object is created.
+
+        //Parameterized Constructor (With parameters)==Used to initialize values when an object is created.
+        
+
+public class Main {
+
+    public static void main(String [] args){
+
+        
+        Human h1= new Human("steve",29,70);
+        Human h2= new Human("Rick",65,60);
+
+        System.out.println(h2.name);
+        h1.drink();
+        h2.eat();
+
+    }
+
+}
