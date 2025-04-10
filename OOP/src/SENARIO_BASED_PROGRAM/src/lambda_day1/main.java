18bcf9fd1f4cf217c6510c86c27b548f22e9de6d
chetan-there <therechetan24@gmail.com>
2025-04-10 16:15:45 +0530

    predicate as parameter

---
+++ OOP/src/SENARIO_BASED_PROGRAM/src/lambda_day1/main.java
@@ -0,0 +1,16 @@
+package OOP.src.SENARIO_BASED_PROGRAM.src.lambda_day1;
+
+public class main {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(" ^");
+                
+            }
+            System.out.println();
+        }
+
+    }
+}
