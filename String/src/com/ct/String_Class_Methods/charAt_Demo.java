26b6f4078b218891c37770d012c3b345b958c46a
chetan-there <therechetan24@gmail.com>
2025-05-11 23:37:47 +0530

    String

---
+++ String/src/com/ct/String_Class_Methods/charAt_Demo.java
@@ -0,0 +1,19 @@
+//Note : It is used to fetch a single character from the given String based on the index position. Method return type is char.
+
+
+
+package com.ct.String_Class_Methods;
+
+public class charAt_Demo {
+
+	public static void main(String[] args) 
+	{
+		   String x = "Hello Hyderabad";
+
+	        char ch1 =  x.charAt(6); //H
+			System.out.println(ch1); 
+
+	}
+
+}
+
